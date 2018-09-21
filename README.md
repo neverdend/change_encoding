2ba0cac24810537dc1d2b66f5f5ff3ba5753a538
chixi.cc <chixi.cc@antfin.com>
2018-09-21 19:07:24 +0800

    Add basic logic.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # change_encoding
-A tool to change file encoding.
+
+## 功能
+遍历「指定目录」下的所有文件，如果文件的后缀名是「指定后缀名」、并且文件的编码是「指定原始编码」，则将文件的编码修改为「指定目标编码」。
+
+## 准备工作
+```shell
+brew install python
+pip2 install chardet
+```
+
+## 使用方式
+1. git clone。
+2. 移动到项目目录。
+3. 修改 change_encoding.py 中的变量值。
+   1. ROOT_PATH：「指定目录」。
+   2. SUFFIX_LIST：「指定后缀名」列表。
+   3. SOURCE_ENCODING_LIST：「指定原始编码」列表。
+   4. TARGET_ENCODING：「指定目标编码」。
+4. python2 change_encoding.py
+
---
+++ change_encoding.py
@@ -0,0 +1,66 @@
+# coding=utf-8
+import os
+from chardet.universaldetector import UniversalDetector
+import codecs
+
+ROOT_PATH = "some_absolute_path"
+SUFFIX_LIST = [".java"]
+SOURCE_ENCODING_LIST = ["gbk", "gb2312", "gb18030"]
+TARGET_ENCODING = "utf-8"
+
+detector = UniversalDetector()
+
+def get_encoding(filepath):
+    with open(filepath, mode="r") as a_file:
+        detector.reset()
+        for line in a_file:
+            detector.feed(line)
+            if detector.done: break
+        detector.close()
+        return detector.result['encoding']
+
+def change_encoding(source_path, target_path, source_encoding, target_encoding):
+    BLOCKSIZE = 1048576
+    with codecs.open(source_path, "r", source_encoding) as source_file:
+        with codecs.open(target_path, "w", target_encoding) as target_file:
+            while True:
+                contents = source_file.read(BLOCKSIZE)
+                if not contents:
+                    break
+                target_file.write(contents)
+
+
+if __name__ == '__main__':
+
+    SUFFIX_LIST = [suffix.lower() for suffix in SUFFIX_LIST]
+    SOURCE_ENCODING_LIST = [encoding.lower() for encoding in SOURCE_ENCODING_LIST]
+    TARGET_ENCODING = TARGET_ENCODING.lower()
+
+    for root, dirs, files in os.walk(ROOT_PATH):
+
+        if not root.endswith("/"):
+            root = root + "/"
+
+        for name in files:
+
+            need_change = False
+            for suffix in SUFFIX_LIST:
+                if name.lower().endswith(suffix):
+                    need_change = True
+                    break
+
+            if not need_change:
+                continue
+
+            fullpath = root + name
+            encoding = get_encoding(fullpath)
+
+            if encoding.lower() not in SOURCE_ENCODING_LIST:
+                continue
+
+            print "change encoding of [%s] from [%s] to [%s]: " % (fullpath, encoding, TARGET_ENCODING),
+            fullpath_bak = fullpath + ".bak"
+            os.rename(fullpath, fullpath_bak)
+            change_encoding(fullpath_bak, fullpath, encoding, TARGET_ENCODING)
+            os.remove(fullpath_bak)
+            print "SUCCESS!"
